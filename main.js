51e721c7eb7648ee5fae138f472735a9bae32774
FelipeBiazin05 <142363711+FelipeBiazin05@users.noreply.github.com>
2024-03-30 20:01:01 -0300

    corrigindo o text-decoration do java script

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
 
-    $('form').on('submit', function(e) {
-        e.preventDefault();
+$('form').on('submit', function(e) {
+    e.preventDefault();
 
-        const tarefa = $('#nomeDaTarefa').val()
-        const novaTarefa = $(`<li><a id="link" href="#"></a></li>`); 
-        $(`<li><a id="link" href="#">${tarefa}</a></li>`).appendTo(novaTarefa);
-        $(novaTarefa).appendTo('ul') ;
-        $('#nomeDaTarefa').val('');
+    const tarefa = $('#nomeDaTarefa').val();
+    const novaTarefa = $(`<li><a class="link" href="#">${tarefa}</a></li>`);
 
-        $('#link').click(function(event) {
-            const novaTarefa = $(`<li><a id="link" href="#"></a></li>`); 
-            event.preventDefault(); 
-                sublinharTexto(novaTarefa);
+    $('ul').append(novaTarefa);
+    $('#nomeDaTarefa').val('');
+
+        $('.link').click(function(event) {
+            event.preventDefault();
+            $(this).css('text-decoration', 'line-through');
         });
-        
-            function sublinharTexto() {
-                let elemento = document.getElementById('link').val();
-                elemento.style.textDecoration=('line-through');
-            }
     });
 });
